b3b8464091b8a927429c382a1c33b71b40c5f58d
TABITA DA SILVA DE ALMEIDA <tabita.almeida@etec.sp.gov.br>
2017-05-07 23:52:21 -0300

    tentando arrumar contato

--- contact.php
+++ contact.php
@@ -1,19 +1,17 @@
 <?php 
 if(isset($_POST['submit'])){
     $to = "almeidatabita@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['name'];
-    //$last_name = $_POST['last_name'];
-    $subject = "Enviado de tabitaalmeida.com";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $from = $_POST['name']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "Contato Site Tábita";
+    $message = $name . " " . " escreveu:" . "\n\n" . $_POST['message'];
     $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Email sended. Thank you " . $name . ", I'll get in touch soon as possible.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
