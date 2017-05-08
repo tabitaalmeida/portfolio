f9a51c0bdab5d0dce79c0516ba771f78ad676697
TABITA DA SILVA DE ALMEIDA <tabita.almeida@etec.sp.gov.br>
2017-05-08 00:04:34 -0300

    arrumando contact tks

--- contactthanks.php
+++ contactthanks.php
@@ -1,34 +1,44 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
-
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-	<title>A Nice & Simple Contact Form</title>
-	
-	<link rel="stylesheet" type="text/css" href="style.css" />
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Tábita Almeida \ Web Designer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Portfólio da Tábita Almeida. Web Designer Freelancer" />
+	<meta name="keywords" content="tabita, almeida, portfolio, web, design, freelancer, " />
+	<meta name="author" content="Tábita Almeida" />
+
+	<!-- CSS STYLES -->
+	<link rel="stylesheet" href="css/flexboxgrid.min.css">
+	<link rel="stylesheet" href="css/style2.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Chonburi" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<!-- PLUGINS -->
+	<script src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.fullPage.min.js"></script>
+
+
 </head>
+
 
 <body>
 
 	<div id="page-wrap">
 
-		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
-		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
-			
-			
-		<br /><br />
-		
 			
 		<h1>Your message has been sent!</h1><br />
 		
-		<p><a href="index.html">Back to Contact Form</a></p>
+		<p><a href="index.html">Back to home</a></p>
 	
 	</div>
 	
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-	</script>
-	<script type="text/javascript">
-	_uacct = "UA-68528-29";
-	urchinTracker();
-	</script>
+	
 
 </body>
 
