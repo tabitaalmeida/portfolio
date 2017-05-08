ce60b81991811b9525c3baee93d9b05836eed802
TABITA DA SILVA DE ALMEIDA <tabita.almeida@etec.sp.gov.br>
2017-05-07 23:54:49 -0300

    index php

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Tábita Almeida \ Web Designer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Portfólio da Tábita Almeida. Web Designer Freelancer" />
+	<meta name="keywords" content="tabita, almeida, portfolio, web, design, freelancer, " />
+	<meta name="author" content="Tábita Almeida" />
+
+	<!-- CSS STYLES -->
+	<link rel="stylesheet" href="css/flexboxgrid.min.css">
+	<link rel="stylesheet" href="css/style2.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Chonburi" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<!-- PLUGINS -->
+	<script src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.fullPage.min.js"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('#fullpage').fullpage({
+				anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+				sectionsColor: ['#1b1a25','#FFF', '#FFF', '#FFF', '#FF5F6D'],
+				navigation: true,
+				navigationPosition: 'right',
+				navigationTooltips: ['Home', 'Pé com Pé', 'Zoombie Lab', 'Bachema', 'Contact']
+			});
+		});
+
+		
+	</script>
+
+</head>
+
+
+<body>
+
+
+ <header>
+  <div class="row">
+  
+  <div class="col-xs-offset-3 col-xs-3">
+  	<input type='checkbox' id='toggle' style='display:none;' />
+  <label class='toggle-btn toggle-btn__cross' for='toggle'>
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+  </label>
+    <nav>
+    <ul class="ftr menu">
+      <li><a rel="nofollow" rel="noreferrer" href="#home">Home</a></li>
+      <li><a rel="nofollow" rel="noreferrer" href="#section0">Work</a></li>
+      <li><a rel="nofollow" rel="noreferrer" href="#section3">Contact</a></li>
+    </ul>
+    <ul class="social">
+    	<li><a href="https://www.facebook.com/TabitaAlmeida" target="_blank"><i class="fa fa-facebook"></i></a></li>
+    	<li><a href="https://www.instagram.com/chatapracomr/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+    	<li><a href="https://www.linkedin.com/in/tabitaalmeida" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+    	<li><a href="https://www.behance.net/tabitaalmeida" target="_blank"><i class="fa fa-behance"></i></a></li>
+    </ul>
+  </nav>
+  </div>
+  
+  </div>
+
+</header>
+
+
+
+<section id="fullpage">
+
+<div class="section" id="home">
+	<div class="row">
+	<div class="col-xs-offset-1 col-xs-10">
+
+	  <div class="box header">
+    <h1 class="ftr">
+      <span>I AM</span>
+      <span>TÁBITA ALMEIDA</span>
+      <span>A FRONTEND</span>
+      <span>& WEB DESIGNER</span>
+    </h1>
+  </div>
+
+
+   <div class="box text">
+    <p><span>Hi!</span> I am Tábita and I'm a 23 years-old web designer who lives in Brazil. I used to work with graphic design, I tried to switch to front end development but I was missing the creativy part, than, I notice that my place were right there in the middle. I love the arts and I love to see things happening, I love to do what I call, handsome technologies.</p>
+
+    <p>Speaking about my personal life, I realy want to travel across the world to know countries and there culture, my biggest dream of all is to do a tour visiting and eating in all the best places of the world. While I can't do that, I work. </p>
+
+   
+  </div>
+
+
+  
+		
+	</div>
+
+
+</div>
+</div>
+
+	<div class="section " id="section0">
+		<div class="intro col-xs-offset-1 col-xs-10">
+			
+
+			<div class="row">
+				<div class="col-xs-6">
+
+				  <div class="boton-linea-ext">
+    <span class="linea1"></span>
+    <span class="linea2"></span>
+    <span class="linea3"></span>
+    <span class="linea4"></span>
+
+    <div class="work"> <h2>Pé com Pé </h2>
+				<p>A children's footwear company, with 35 years of tradition and the market in Brazil and abroad, now with the virtual store project. In this project I was responsible for the entire store, since the visual part, as solutions to increase sales. </p>
+				<ul>
+					<li><strong>Project:</strong> Website</li>
+					<li><strong>Client:</strong> Pé com Pé</li>
+					<li><strong>Agency:</strong> A&T</li>
+					<li><strong>Function:</strong> Web Design & Front-End Development</li>
+				</ul>
+				<a href="pecompe.html"><button>SEE MORE</button></a> </div>
+   
+  </div>
+
+				
+				</div>
+				<div class="col-xs-6 img"><img src="img/pecompe.png" alt=""></div>
+			</div>
+
+			 
+
+
+  
+
+
+			
+			
+		</div>
+	</div>
+	
+
+		<div class="section " id="section1">
+		<div class="intro col-xs-offset-1 col-xs-10">
+			
+
+			<div class="row">
+				<div class="col-xs-6">
+
+				  <div class="boton-linea-ext">
+    <span class="linea1"></span>
+    <span class="linea2"></span>
+    <span class="linea3"></span>
+    <span class="linea4"></span>
+
+    <div class="work"> <h2>Zoombie Lab </h2>
+				<p>We went through a rebuilding process of our visual identity, we developed a new logo to represent the Zoombie as well as the idea that we had when it was created. The idea for the name came up by like we have for different things, creative, a little influence of Thriller and science. And that's where the Creative Lab, nicknamed affectionately of Lab a place where we can test any crazy idea that comes into your head. And it was our laboratory that left the powerful virus of creativity and innovation in design. The virus zoombie, which aims to grow and spread between ideas and different thoughts.</p>
+				<ul>
+					<li><strong>Project:</strong> Website</li>
+					<li><strong>Client:</strong> Zoombie Lab</li>
+					<li><strong>Agency:</strong> Zoombie Lab</li>
+					<li><strong>Function:</strong> Web Design & Front-End Development</li>
+				</ul>
+				<a href="zoombie.html" target="_blank"><button>SEE MORE</button></a> </div>
+   
+  </div>
+
+				
+				</div>
+				<div class="col-xs-6"><img src="img/zoombie.png" alt=""></div>
+			</div>
+
+			 
+
+
+  
+
+
+			
+			
+		</div>
+	</div>
+
+		<div class="section " id="section2">
+		<div class="intro col-xs-offset-1 col-xs-10">
+			
+
+			<div class="row">
+				<div class="col-xs-6">
+
+				  <div class="boton-linea-ext">
+    <span class="linea1"></span>
+    <span class="linea2"></span>
+    <span class="linea3"></span>
+    <span class="linea4"></span>
+
+    <div class="work"> <h2>Bachema </h2>
+				<p>Bachema is a laboratory that specializes in physical-chemical and environmental microbiology analysis of water, soil, air and waste.</p>
+				<p>We offer detailed exams of organic and inorganic compounds, microbiological and ecotoxicological, measurement of asbestos in materials, and the study of soil treatment and pilot tests for biogas and biodiesel production.</p>
+				<ul>
+					<li><strong>Project:</strong> Website</li>
+					<li><strong>Client:</strong> Bachema</li>
+					<li><strong>Agency:</strong> MOQUE.CA</li>
+					<li><strong>Function:</strong> Front-End Development</li>
+				</ul>
+				<a href="http://www.bachema.com.br/" target="_blank"><button>SEE MORE</button></a> </div>
+   
+  </div>
+
+				
+				</div>
+				<div class="col-xs-6 img"><img src="img/bachema.png" alt=""></div>
+			</div>
+
+			 
+
+
+  
+
+
+			
+			
+		</div>
+	</div>
+
+		<div class="section contact" id="section3">
+		<div class="intro col-xs-offset-1 col-xs-10">
+			
+
+			<div class="row">
+				<div class="col-xs-10 col-xs-offset-1">
+
+				<h2 class="ftr">Wanna talk?</h2>
+
+				<form action="contact.php" class="contact" method="post">
+					<input type="text" name="name" placeholder="Name:">
+					<input type="email" name="email" placeholder="E-mail:">
+					<textarea name="message" placeholder="Message:" id="" cols="30" rows="10"></textarea>
+					<input type="submit" value="SEND">
+				</form>
+
+				<h4>You can also find me in +55 11 9 72854871</h4>
+
+				
+				</div>
+			
+			</div>
+
+			 
+
+
+  
+
+
+			
+			
+		</div>
+	</div>
+</section>
+
+
+<footer class="footer"><p>DESIGNED BY TÁBITA ALMEIDA</p></footer>	
+</body>
+</html>
